96e46e810bf6920e894531f7e28c427e7cae6b8b
GrossWhiteRabbit <vowa.kononenko@gmail.com>
2021-02-10 19:12:50 +0200

    Updates

---
+++ precache-manifest.165405625a72bf2429b9b6728a88ff47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f693006348067a829708e5d1077c7e",
+    "url": "/firstReactSPA__miniTwiter/index.html"
+  },
+  {
+    "revision": "e40cd347bd37dbbf16b8",
+    "url": "/firstReactSPA__miniTwiter/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5ee863070c5290c59fb0",
+    "url": "/firstReactSPA__miniTwiter/static/css/main.69cbd2c1.chunk.css"
+  },
+  {
+    "revision": "e40cd347bd37dbbf16b8",
+    "url": "/firstReactSPA__miniTwiter/static/js/2.c7952e04.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/firstReactSPA__miniTwiter/static/js/2.c7952e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee863070c5290c59fb0",
+    "url": "/firstReactSPA__miniTwiter/static/js/main.e8e900b8.chunk.js"
+  },
+  {
+    "revision": "46facdf8fc96a5106f4d",
+    "url": "/firstReactSPA__miniTwiter/static/js/runtime-main.98c0e488.js"
+  }
+]);
